db36181396fa79e4f7e7ed6228336ccd86ed3532
James Halliday <mail@substack.net>
2011-11-17 06:01:50 -0800

    abstracted out service info/refs

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('qs');
+var path = require('path');
 var spawn = require('child_process').spawn;
 
-var server = http.createServer(function (req, res) {
-    function cache () {
+var config = {
+    repos : __dirname + '/repos',
+    services : [ 'upload-pack', 'receive-pack' ]
+};
+
+http.createServer(function (req, res) {
+    var u = url.parse(req.url);
+    var params = qs.parse(u.query);
+    
+    function noCache () {
         res.setHeader('expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
         res.setHeader('pragma', 'no-cache');
         res.setHeader('cache-control', 'no-cache, max-age=0, must-revalidate');
     }
     
     console.dir(req.method + ' ' + req.url);
-    if (req.method === 'GET'
-    && req.url === '/bouncy/info/refs?service=git-receive-pack') {
+    
+    var m;
+    if (req.method === 'GET' && (m = req.url.match('/([^\/]+)/info/refs'))) {
+        var repo = m[1];
+        
+        if (!params.service) {
+            res.statusCode = 400;
+            res.end('service parameter required');
+            return;
+        }
+        
+        var service = params.service.replace(/^git-/, '');
+        
+        if (config.services.indexOf(service) < 0) {
+            res.statusCode = 405;
+            res.end('service not available');
+            return;
+        }
+        
         res.setHeader('content-type',
-            'application/x-git-receive-pack-advertisement'
+            'application/x-git-' + service + '-advertisement'
         );
-        cache();
+        noCache();
         
         function pack (s) {
             var n = (4 + s.length).toString(16);
             return Array(4 - n.length + 1).join('0') + n + s;
         }
-        res.write(pack('# service=git-receive-pack\n'));
+        res.write(pack('# service=git-' + service + '\n'));
         res.write('0000');
         
-        var ps = spawn('git-receive-pack', [
+        var ps = spawn('git-' + service, [
             '--stateless-rpc',
             '--advertise-refs',
-            'bouncy_repo',
+            path.join(config.repos, repo),
         ]);
         ps.stdout.pipe(res);
         ps.stderr.pipe(process.stderr, { end : false });
@@ -38,7 +65,7 @@ var server = http.createServer(function (req, res) {
     }
     else if (req.method === 'POST' && req.url === '/bouncy/git-receive-pack') {
         res.setHeader('content-type', 'application/x-git-receive-pack-result');
-        cache();
+        noCache();
         
         var ps = spawn('git-receive-pack', [
             '--stateless-rpc',
@@ -48,6 +75,8 @@ var server = http.createServer(function (req, res) {
         req.pipe(ps.stdin);
         ps.stderr.pipe(process.stderr, { end : false });
     }
-});
-
-server.listen(7000);
+    else {
+        res.statusCode = 404;
+        res.end('not found');
+    }
+}).listen(7000);
